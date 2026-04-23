9586dd6268179e03cc61f680b45cfd7afa1b0eda
chasedifuke-ai <chasedifuke@gmail.com>
2026-04-22 22:08:55 -0400

    Update kdf.js

--- 02-js/kdf.js
+++ 02-js/kdf.js
@@ -1,1 +1,23 @@
-// kdf.js — valorant page scripts
+
+    function calculate(){
+
+    let k = document.getElementById("kills").value;
+    k = Number(k);
+
+    let d = document.getElementById("deaths").value;
+    d = Number(d);
+
+    let a = document.getElementById("assists").value;
+    a = Number(a);
+
+    let result;
+
+    if(d == 0){
+    result = k + a;
+} else {
+    result = (k + a) / d;
+}
+
+    document.getElementById("result").innerHTML = "Your K/D/A is :" +result;
+}
+
